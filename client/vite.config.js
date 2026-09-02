7650260a9c449359c735a4b5ee7b633bac1ce11b
rethish2828 <rethish2828@gmail.com>
2026-09-02 06:48:54 +0530

    Update vite config base path for GitHub Pages

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/photobooth/',
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': 'http://localhost:3001',
-      '/uploads': 'http://localhost:3001'
-    }
+    proxy: {}
   }
 });
